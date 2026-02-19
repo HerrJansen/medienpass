687827ad5720c1d5da5125f11d2ece927311ba7f
HerrJansen <thomas.jansen@thomaeum.de>
2026-02-19 11:57:35 +0100

    Create index.js

---
+++ frontend/index.js
@@ -0,0 +1,5 @@
+import App from "./App.js";
+
+const rootElement = document.getElementById("root");
+rootElement.innerHTML = "";
+rootElement.appendChild(document.createTextNode("App konnte noch nicht gerendert werden."));
